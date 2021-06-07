5db5e4309abf6da33a012dfe4f59ad8cc401e2b7
EmanOthman21 <eothman21@gmail.com>
2021-06-07 11:24:51 +0200

    Add: Slider column

---
+++ src/components/ImageSlider.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="slider"></div>
+</template>
+
+<script>
+export default {
+  name: "ImageSlider",
+  data: function () {
+    return {};
+  },
+  created: function () {},
+  computed: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.slider {
+  width: 50vw;
+  height: 100vh;
+  background-color: #012376;
+  @media (max-width: 992px) {
+    display: none;
+  }
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="login">
-    <h1>Login</h1>
+    <ImageSlider />
   </div>
 </template>
 
 <script>
+import ImageSlider from "../components/ImageSlider.vue";
 export default {
   name: "Login",
+  components: {
+    ImageSlider,
+  },
 };
 </script>
+
+<style lang="scss" scoped></style>
